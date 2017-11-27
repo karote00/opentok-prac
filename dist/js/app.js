f458c7854c67ed1690f40de7e25869c10d3a3dac
Asa Tsai <karoteglathe@gmail.com>
2017-11-27 11:04:22 +0800

    Initial dist subtree commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -16,6 +16,34 @@ function initializeSession() {
   // Subscribe to a newly created stream
 
   // Create a publisher
+ //  var publisher = OT.initPublisher('publisher', {
+ //    insertMode: 'append',
+ //    width: '100%',
+ //    height: '100%'
+ //  }, handleError);
+
+ //  // Connect to the session
+ //  session.connect(token, function(error) {
+ //    // If the connection is successful, publish to the session
+ //    if (error) {
+ //      handleError(error);
+ //    } else {
+ //      session.publish(publisher, handleError);
+ //    }
+ //  });
+
+ //  session.on('streamCreated', function(event) {
+	//   session.subscribe(event.stream, 'subscriber', {
+	//     insertMode: 'append',
+	//     width: '100%',
+	//     height: '100%'
+	//   }, handleError);
+	// });
+}
+
+function publisher() {
+  var session = OT.initSession(apiKey, sessionId);
+	// Create a publisher
   var publisher = OT.initPublisher('publisher', {
     insertMode: 'append',
     width: '100%',
@@ -23,6 +51,19 @@ function initializeSession() {
   }, handleError);
 
   // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+}
+
+function subscriber() {
+  var session = OT.initSession(apiKey, sessionId);
+	// Connect to the session
   session.connect(token, function(error) {
     // If the connection is successful, publish to the session
     if (error) {
